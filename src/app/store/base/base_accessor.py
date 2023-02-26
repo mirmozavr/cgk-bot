a1cd09cba95afc01b93a61194686d7583bf04999
Mo (laptop) <email>
2023-02-26 18:43:15 +0500

    Add store, add base_accessor.py

---
+++ src/app/store/base/base_accessor.py
@@ -0,0 +1,17 @@
+import typing as t
+
+if t.TYPE_CHECKING:
+    from src.web.app import Application
+
+
+class BaseAccessor:
+    def __init__(self, app: "Application", *args, **kwargs):
+        self.app = app
+        app.on_startup.append(self.connect)
+        app.on_cleanup.append(self.disconnect)
+
+    async def connect(self, app: "Application"):
+        raise NotImplementedError
+
+    async def disconnect(self, app: "Application"):
+        raise NotImplementedError
