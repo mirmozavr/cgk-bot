7d735a640eb4986fbf8296ce06b0ed84f2b9b718
Mo (laptop) <email>
2023-02-27 23:30:47 +0500

    Add quiz model

---
+++ src/app/quiz/models.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from sqlalchemy import Integer, Column, String
+
+from src.app.store.database import Base
+
+
+@dataclass
+class Question:
+    id: Optional[int]
+    title: str
+    answer: str
+
+
+class QuestionModel(Base):
+    __tablename__ = "questions"
+    id = Column(Integer, primary_key=True)
+    title = Column(String, unique=True, nullable=False)
+    answer = Column(String, unique=False, nullable=False)
+
+    def __repr__(self):
+        return f"QuestionModel(id={self.id!r}, title={self.title!r}, title={self.answer!r})"
--- src/app/store/database/__init__.py
+++ src/app/store/database/__init__.py
@@ -2,3 +2,4 @@
 from src.app.store.database.sqlalchemy_base import Base
 from src.app.store.database.database import Database
 from src.app.admin.models import *
+from src.app.quiz.models import *
