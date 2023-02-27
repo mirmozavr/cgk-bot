566e8570d057754fb8e03fc2d798375d0d87de5c
Mo (laptop) <email>
2023-02-27 18:54:16 +0500

    Fix fernet key read

--- src/app/web/session.py
+++ src/app/web/session.py
@@ -3,13 +3,12 @@ import typing as t
 
 from aiohttp_session import setup
 from aiohttp_session.cookie_storage import EncryptedCookieStorage
-from cryptography import fernet
 
 if t.TYPE_CHECKING:
     from src.app.web.app import Application
 
 
 def setup_session(app: "Application"):
-    fernet_key = fernet.Fernet.generate_key()
+    fernet_key = app.config.session.key
     secret_key = base64.urlsafe_b64decode(fernet_key)
     setup(app, EncryptedCookieStorage(secret_key))
