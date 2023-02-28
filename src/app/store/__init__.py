d48c4f0929ea0a1364dfbbf7c0d193a1b2a67c9e
Mo (laptop) <email>
2023-03-01 00:30:14 +0500

    Add tg dataclasses

--- src/app/store/__init__.py
+++ src/app/store/__init__.py
@@ -9,14 +9,14 @@ if t.TYPE_CHECKING:
 class Store:
     def __init__(self, app: "Application"):
         from src.app.store.admin.accessor import AdminAccessor
-        # from src.app.store.bot.manager import BotManager
-        # from src.app.store.quiz.accessor import QuizAccessor
-        # from src.app.store.vk_api.accessor import VkApiAccessor
+        # from src.app.store.quiz.accessor import QuizAccessor # Resolve merge conflict
+        from src.app.bot.manager import BotManager
+        from src.app.store.tg_api.accessor import TgApiAccessor
 
         self.admins = AdminAccessor(app)
-        # self.quizzes = QuizAccessor(app)
-        # self.vk_api = VkApiAccessor(app)
-        # self.bots_manager = BotManager(app)
+        self.bot_manager = BotManager(app)
+        # self.quiz = QuizAccessor(app) # Resolve merge conflict
+        self.tg_api = TgApiAccessor(app)
 
 
 def setup_store(app: "Application"):
---
+++ src/app/store/tg_api/dataclasses.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class User:
+    id: int
+    is_bot: bool
+    first_name: str
+    username: str = None
+    language_code: str = None
+
+
+@dataclass
+class Message:
+    message_id: int
+    text: str = None
+    date: int = None
+    user: User = None
+
+
+@dataclass
+class Chat:
+    id: int
+    type: str
+    first_name: str = None
+    username: str = None
+    title: str = None
+
+
+@dataclass
+class Update:
+    id: int
+    message: Message
