4dd9617f9d9e67682c65e81e6b792deaeefcd8c9
Mo (laptop) <email>
2023-03-05 23:40:17 +0500

    Fix accessors

--- src/app/store/quiz/accessor.py
+++ src/app/store/quiz/accessor.py
@@ -1,12 +1,13 @@
 from sqlalchemy import select
 
+from src.app.bot.models import GameModel
 from src.app.store.base.base_accessor import BaseAccessor
 from src.app.quiz.models import QuestionModel, Question
 
 
 class QuizAccessor(BaseAccessor):
     async def create_question(self, title: str, answer: str) -> Question:
-        async with self.app.database.session as session:
+        async with self.app.database.session.begin() as session:
             question = QuestionModel(
                 title=title,
                 answer=answer,
@@ -16,17 +17,23 @@ class QuizAccessor(BaseAccessor):
 
     async def get_question_by_title(self, title: str) -> Question | None:
         stmt = select(QuestionModel).where(QuestionModel.title == title)
-        async with self.app.database.session as session:
+        async with self.app.database.session.begin() as session:
             result = await session.execute(stmt)
             data = result.scalars().all()
             if data:
                 data = data[0]
                 return Question(data.id, data.title, data.answer)
 
+    async def get_question_by_id(self, id: int) -> QuestionModel | None:
+        stmt = select(QuestionModel).where(QuestionModel.id == int(id))
+        async with self.app.database.session.begin() as session:
+            result = await session.scalars(stmt)
+            return result.one_or_none()
+
     async def list_questions(self) -> list[Question]:
         stmt = select(QuestionModel)
 
-        async with self.app.database.session as session:
+        async with self.app.database.session.begin() as session:
             result = await session.execute(stmt)
             questions = result.scalars().all()
             return [
@@ -37,3 +44,20 @@ class QuizAccessor(BaseAccessor):
                 )
                 for q in questions
             ]
+
+    async def get_question_for_game(self, game: GameModel):
+        stmt = (
+            select(QuestionModel)
+            .where(QuestionModel.id.notin_(list(map(int, game.q_history_to_list()))))
+            .limit(1)
+        )
+        async with self.app.database.session.begin() as session:
+            result = await session.scalars(stmt)
+            question = result.one_or_none()
+            if question is None:
+                game.clear_history()
+                return await self.get_question_for_game(game)
+            return question
+
+
+
--- src/app/store/tg_api/accessor.py
+++ src/app/store/tg_api/accessor.py
@@ -1,12 +1,13 @@
 import json
 import typing as t
-from pprint import pprint
 from typing import Optional, List
 
 from aiohttp.client import ClientSession
+from sqlalchemy import select
 
+from src.app.bot.models import GameModel, PlayerModel
 from src.app.store.base.base_accessor import BaseAccessor
-from src.app.store.tg_api.dataclasses import Message, Update, Chat, User
+from src.app.store.tg_api.dataclasses import Message, Update, Chat, User, CallbackQuery
 from src.app.store.tg_api.poller import Poller
 
 if t.TYPE_CHECKING:
@@ -41,26 +42,23 @@ class TgApiAccessor(BaseAccessor):
         """
         params = {
             "offset": self.offset or "",
-            "timeout": 5,
-            "allowed_updates": ["message"],
+            "timeout": 60,
+            "allowed_updates": json.dumps(["message", "callback_query"]),
         }
         get_updates_url = f"{self.base_url}/getUpdates"
         async with self.session.get(get_updates_url, params=params) as response:
             data = await response.json()
-            pprint(data)
             for raw_update in data["result"]:
                 # process only group messages
-                if (
-                        raw_update.get("my_chat_member")
-                        or raw_update["message"]["chat"]["type"] != "group"
-                        or not raw_update["message"].get("text")
-                ):
+                if ("message" in raw_update) and (raw_update["message"]["chat"]["type"] != "group"
+                                                  or not raw_update["message"].get("text")):
                     self.offset = raw_update["update_id"] + 1
                     continue
                 update = self.dict_to_dc(raw_update)
 
                 await self.app.store.bot_manager.handle_update(update)
 
+                # Increment offset by 1 to mark updates as accepted
                 self.offset = update.update_id + 1
 
     async def set_initial_commands(self) -> None:
@@ -72,6 +70,7 @@ class TgApiAccessor(BaseAccessor):
         commands = {
             "about": "Info",
             "rules": "Game rules",
+            "team_up": "Gather the team",
             "start_game": "Start game",
             "end_game": "End game",
         }
@@ -104,25 +103,56 @@ class TgApiAccessor(BaseAccessor):
         :param raw_data: Dict object received from getUpdates
         :return: Message class object with nested Chat, User, Message dataclasses
         """
-        return Update(
-            update_id=raw_data["update_id"],
-            message=Message(
-                message_id=raw_data["message"]["message_id"],
-                date=raw_data["message"]["date"],
-                text=raw_data["message"]["text"],
-                chat=Chat(
-                    id=raw_data["message"]["chat"]["id"],
-                    type=raw_data["message"]["chat"]["type"],
-                    title=raw_data["message"]["chat"]["title"],
+        if "message" in raw_data:
+            return Update(
+                update_id=raw_data["update_id"],
+                message=Message(
+                    message_id=raw_data["message"]["message_id"],
+                    date=raw_data["message"]["date"],
+                    text=raw_data["message"]["text"],
+                    chat=Chat(
+                        id=raw_data["message"]["chat"]["id"],
+                        type=raw_data["message"]["chat"]["type"],
+                        title=raw_data["message"]["chat"]["title"],
+                    ),
+                    user=User(
+                        id=raw_data["message"]["from"]["id"],
+                        is_bot=raw_data["message"]["from"]["is_bot"],
+                        first_name=raw_data["message"]["from"]["first_name"],
+                        username=raw_data["message"]["from"].get("username"),
+                    ),
                 ),
-                user=User(
-                    id=raw_data["message"]["from"]["id"],
-                    is_bot=raw_data["message"]["from"]["is_bot"],
-                    first_name=raw_data["message"]["from"]["first_name"],
-                    username=raw_data["message"]["from"].get("username"),
-                ),
-            ),
-        )
+            )
+        if "callback_query" in raw_data:
+            return Update(
+                update_id=raw_data["update_id"],
+                callback_query=CallbackQuery(
+                    id=raw_data["callback_query"]["id"],
+                    data=raw_data["callback_query"].get("data"),
+                    user=User(
+                        id=raw_data["callback_query"]["from"]["id"],
+                        is_bot=raw_data["callback_query"]["from"]["is_bot"],
+                        first_name=raw_data["callback_query"]["from"]["first_name"],
+                        username=raw_data["callback_query"]["from"].get("username"),
+                    ),
+                    message=Message(
+                        message_id=raw_data["callback_query"]["message"]["message_id"],
+                        text=raw_data["callback_query"]["message"].get("text"),
+                        date=raw_data["callback_query"]["message"].get("date"),
+                        user=User(
+                            id=raw_data["callback_query"]["message"]["from"]["id"],
+                            is_bot=raw_data["callback_query"]["message"]["from"]["is_bot"],
+                            first_name=raw_data["callback_query"]["message"]["from"]["first_name"],
+                            username=raw_data["callback_query"]["message"]["from"].get("username"),
+                        ),
+                        chat=Chat(
+                            id=raw_data["callback_query"]["message"]["chat"]["id"],
+                            type=raw_data["callback_query"]["message"]["chat"]["type"],
+                            title=raw_data["callback_query"]["message"]["chat"]["title"],
+                        )
+                    ),
+                )
+            )
 
     async def send_message(self, chat_id: int, text: str) -> None:
         """
@@ -131,9 +161,145 @@ class TgApiAccessor(BaseAccessor):
         :param text: Text to send to chat
         :return:
         """
+        url = f"{self.base_url}/sendMessage"
         params = {
             "chat_id": chat_id,
             "text": text,
         }
+        resp = await self.session.get(url, params=params)
+        return (await resp.json())["result"]["date"]
+
+    async def reply_to_message(self, message: Message, text: str) -> None:
+        """
+        Reply to text message
+        :param message: Message object
+        :param text: Text to send to chat
+        :return:
+        """
+        params = {
+            "chat_id": message.chat.id,
+            "text": text,
+            "reply_to_message_id": message.message_id,
+        }
         snd_msg_url = f"{self.base_url}/sendMessage"
         await self.session.get(snd_msg_url, params=params)
+
+    async def send_inline_button(self, message: Message, data: str):
+        url = f"{self.base_url}/sendMessage"
+        params = {
+            "chat_id": message.chat.id,
+            "text": "Press the button to join THE TEAM",
+            "reply_markup": json.dumps(
+                {
+                    "inline_keyboard": [[{"text": "Join the team",
+                                          "callback_data": data}]],
+                    "resize_keyboard": True,
+                    "one_time_keyboard": True,
+                }
+            ),
+        }
+        await self.session.get(url, params=params)
+
+    async def send_choose_responder_buttons(self, game: GameModel, text: str) -> int:
+        url = f"{self.base_url}/sendMessage"
+        params = {
+            "chat_id": game.id,
+            "text": text,
+            "reply_markup": json.dumps(
+                {
+                    "keyboard": [[{"text": f"{player.first_name}"}
+                                  for player in await self.get_team_players_models(game)]],
+                    "resize_keyboard": True,
+                    "one_time_keyboard": True,
+                }
+            ),
+        }
+        resp = await self.session.get(url, params=params)
+        return (await resp.json())["result"]["date"]
+
+    async def remove_buttons(self, game: GameModel, text: str) -> int:
+        url = f"{self.base_url}/sendMessage"
+        params = {
+            "chat_id": game.id,
+            "text": text,
+            "reply_markup": json.dumps({"remove_keyboard": True}),
+        }
+        resp = await self.session.get(url, params=params)
+        return (await resp.json())["result"]["date"]
+
+    async def answer_cq(self, cq: CallbackQuery, text: str):
+        url = f"{self.base_url}/answerCallbackQuery"
+        params = {
+            "callback_query_id": cq.id,
+            "text": text,
+            "show_alert": "false",
+        }
+        await self.session.get(url, params=params)
+
+    async def get_game_by_message(self, message: Message) -> Optional[GameModel]:
+        """
+        Get game from db and return Game dataclass object
+        :param message: Message dataclass object
+        :return: Game dataclass object
+        """
+        stmt = (
+            select(GameModel)
+            .where(GameModel.id == message.chat.id)
+        )
+        async with self.app.database.session.begin() as session:
+            result = await session.scalars(stmt)
+            return result.one_or_none()
+
+    async def create_game_by_message(self, message: Message) -> GameModel:
+        """
+        Create game object at db by received message
+        :param message: Message dataclass object
+        :return: GameModel object
+        """
+        game_model = GameModel(
+            id=message.chat.id,
+            update_time=message.date,
+        )
+        async with self.app.database.session.begin() as session:
+            session.add(game_model)
+            return game_model
+
+    async def create_player_by_message(self, message: Message) -> PlayerModel:
+        player_model = PlayerModel(
+            id=message.user.id,
+            username=message.user.username if message.user.username else None,
+            first_name=message.user.first_name,
+        )
+        async with self.app.database.session.begin() as session:
+            session.add(player_model)
+            return player_model
+
+    async def get_capitan(self, game: GameModel) -> Optional[PlayerModel]:
+        stmt = (
+            select(PlayerModel)
+            .where(PlayerModel.id == int(game.cap_id))
+            )
+        async with self.app.database.session.begin() as session:
+            result = await session.scalars(stmt)
+            return result.one_or_none()
+
+    async def get_player_by_name(self, game: GameModel, name: str) -> Optional[PlayerModel]:
+        stmt = (
+            select(PlayerModel)
+            .where(
+                (PlayerModel.first_name == name)
+                & (PlayerModel.id.in_(map(int, game.team_to_list())))
+            )
+        )
+        async with self.app.database.session.begin() as session:
+            result = await session.scalars(stmt)
+            return result.one_or_none()
+
+    async def get_team_players_models(self, game: GameModel):
+        stmt = (
+            select(PlayerModel)
+            .where(PlayerModel.id.in_(map(int, game.team_to_list())))
+        )
+        async with self.app.database.session.begin() as session:
+            result = await session.scalars(stmt)
+            return result.all()
